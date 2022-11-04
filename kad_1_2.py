7056b3ad8945ad0a759bde5dd6d9555014275082
242456 Łaski Robert <242456@edu.p.lodz.pl>
2022-11-04 21:00:27 +0100

    poczatek 1_2

--- kad_1_2.py
+++ kad_1_2.py
@@ -1,4 +1,13 @@
 f = open(r"C:\Users\rober\OneDrive\Pulpit\data.csv")
 lines = f.readlines()
-min_dzialki_kielicha = lines[0][0]+lines[0][1]+lines[0][1]
-for x in len(lines) :
+f.close()
+a = lines[0][0]+lines[0][1]+lines[0][2]
+min_dzialki_kielicha = float(a)
+max_dzialki_kielicha = float(a)
+for x in range(0, len(lines)) :
+    b = lines[x][0] + lines[x][1] + lines[x][2]
+    b = float(b)
+    if(b<min_dzialki_kielicha) : min_dzialki_kielicha = b
+    if(b>max_dzialki_kielicha) : max_dzialki_kielicha = b
+
+print("Max_dzialki_kielicha = ", max_dzialki_kielicha, "     Min_dzialki_kielicha = ", min_dzialki_kielicha)
