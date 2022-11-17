e967709ce39c77c08cf4193edc17a2098c70ea72
242456 Łaski Robert <242456@edu.p.lodz.pl>
2022-11-17 21:03:50 +0100

    Działa, ale kod do dupu, ale trochę lepiej  (:(

--- kad.py
+++ kad.py
@@ -3,85 +3,90 @@ from matplotlib import pyplot as plt
 import math
 
 
-def min(kwiatki, rodzaj_wielkosci):
-    min = float(kwiatki[0][rodzaj_wielkosci])
+def findMinimum(table, column):
+    min = float(table[0][column])
     for x in kwiatki:
-        if float(x[rodzaj_wielkosci]) < min:
-            min = float(x[rodzaj_wielkosci])
+        if float(x[column]) < min:
+            min = float(x[column])
     return min
 
 
-def max(kwiatki, rodzaj_wielkosci):
-    max = float(kwiatki[0][rodzaj_wielkosci])
+def findMaximum(table, column):
+    max = float(table[0][column])
     for x in kwiatki:
-        if (float(x[rodzaj_wielkosci]) > max):
-            max = float(x[rodzaj_wielkosci])
+        if (float(x[column]) > max):
+            max = float(x[column])
     return max
 
 
-def liczebnosc():
-    f = open(r"data.csv")
-    lines = f.readlines()
-    setosa = 0
-    versicolor = 0
-    virginica = 0
-    for i in range(0, len(lines)):
-        if (lines[i][-2] == '0'): setosa = setosa + 1
-        if (lines[i][-2] == '1'): versicolor = versicolor + 1
-        if (lines[i][-2] == '2'): virginica = virginica + 1
-    all = setosa + versicolor + virginica
-    print("Setosa: ", setosa, "(", round(setosa / all * 100, 1), "%)"
-                                                                 "     Versicolor: ", versicolor, "(",
-          round(versicolor / all * 100, 1), "%)"
-                                            "   Virginica: ", virginica, "(", round(virginica / all * 100, 1), "%)")
-
-    f.close()
+def findQuantity(table, column, searched_value):
+    quantity_searched_value = 0
+    for x in table:
+        if (int(x[column]) == searched_value):
+            quantity_searched_value = quantity_searched_value+1
+    return quantity_searched_value
 
 
-def srednia_arytmetyczna(kwiatki, rodzaj_wielkosci):
-    suma = 0.0
-    for i in range(150):
-        suma += float(kwiatki[i][rodzaj_wielkosci])
-    return (suma / 150)
+def findAverage(table, column):
+    sum = 0.0
+    for x in table:
+        sum += float(x[column])
+    return (sum / len(table))
 
 
-def mediana(kwiatki, rodzaj_wielkosci):
-    tablica_wielkosci = []
-    for x in kwiatki:
-        tablica_wielkosci.append(float(x[rodzaj_wielkosci]))
+def findMedian(table, column):
+    table_to_sort = []
+    for x in table:
+        table_to_sort.append(float(x[column]))
+    table_to_sort.sort()
+    middle_value = int(len(table_to_sort)/2)
+    if(len(table_to_sort) % 2 == 0):
+        return ((table_to_sort[middle_value-1]+table_to_sort[middle_value])/2)
+    if(len(table_to_sort) % 2 == 1):
+        return middle_value;
 
-    tablica_wielkosci.sort()
-
-    return ((tablica_wielkosci[74] + tablica_wielkosci[75]) / 2)
 
 
-def kwartylQ1(kwiatki, rodzaj_wielkosci):
+
+def findQ1(table, column):
     Q1 = []
-    # dokladnie 75 elementow
-    for x in kwiatki:
-        Q1.append(float(x[rodzaj_wielkosci]))
-    n = len(kwiatki)
+    for x in table:
+        Q1.append(float(x[column]))
+    n = len(table)
     Q1.sort()
-    return Q1[(n + 1) // 4]
+    if (len(Q1) % 4 == 0):
+        return Q1[int(len(Q1)/4)]-(Q1[int(len(Q1)/4)]-Q1[int(len(Q1)/4)-1])/4
+    if (len(Q1) % 4 == 1):
+        return Q1[int(len(Q1)/4)]
+    if (len(Q1) % 4 == 2):
+        return Q1[int(len(Q1)/4)]+(Q1[int(len(Q1)/4)+1]-Q1[int(len(Q1)/4)])/4
+    if (len(Q1) % 4 == 3):
+        return (Q1[int(len(Q1)/4)]+Q1[int(len(Q1)/4)+1])/2
 
 
-def kwartylQ3(kwiatki, rodzaj_wialkosci):
+def findQ3(table, column):
     Q3 = []
-    for x in kwiatki:
-        Q3.append(float(x[rodzaj_wialkosci]))
+    for x in table:
+        Q3.append(float(x[column]))
 
     Q3.sort()
-    n = len(kwiatki)
-    return Q3[((n + 1) // 4) * 3]
+    if (len(Q3) % 4 == 0):
+        return Q3[int(len(Q3) / 4*3)] - (Q3[int(len(Q3) / 4*3)] - Q3[int(len(Q3) / 4*3) - 1]) / 4*3
+    if (len(Q3) % 4 == 1):
+        return Q3[int(len(Q3) / 4*3)]
+    if (len(Q3) % 4 == 2):
+        return Q3[int(len(Q3) / 4*3)] - (Q3[int(len(Q3) / 4*3)] - Q3[int(len(Q3) / 4*3)-1]) / 4
+    if (len(Q3) % 4 == 3):
+        return (Q3[int(len(Q3) / 4*3)] + Q3[int(len(Q3) / 4*3) - 1]) / 2
 
 
-def odchylenieStandardowe(kwiatki, rodzaj_wielkosci):
-    srednia = float(srednia_arytmetyczna(kwiatki, rodzaj_wielkosci))
+def findStdDeviation(table, column):
+    avg = float(findAverage(table, column))
     sum = 0
-    for x in kwiatki:
-        sum += (float(float(x[rodzaj_wielkosci]) - srednia) ** 2)
+    for x in table:
+        sum += (float(float(x[column]) - avg) ** 2)
 
-    sum /= len(kwiatki)
+    sum /= len(table)
     sum = sum ** 0.5
     return sum
 
@@ -97,24 +102,22 @@ def rysujHistogram(kwiatki):
         dlugosc_platka.append(float(x[2]))
         szerokosc_platka.append(float(x[3]))
 
-
     a = round(min(kwiatki, 0), 0)
     b = round(max(kwiatki, 0), 0)
     bins = int(round(math.sqrt(len(dlugosc_dzialki)), 0))
-    plt.hist(dlugosc_dzialki, bins=bins, range=(a,b), edgecolor='black')
+    plt.hist(dlugosc_dzialki, bins=bins, range=(a, b), edgecolor='black')
     plt.title("Długość działki kielicha")
     plt.xlabel("Długość[cm]")
     plt.ylabel("Liczebność")
     plt.show()
 
-    a = round(min(kwiatki,1), 0)
-    b = round(max(kwiatki,1), 0)
+    a = round(min(kwiatki, 1), 0)
+    b = round(max(kwiatki, 1), 0)
     plt.hist(szerokosc_dzialki, bins=bins, range=(a, b), edgecolor='black')
     plt.title("Szerokość działki kielicha")
     plt.xlabel("Szerokość[cm]")
     plt.ylabel("Liczebność")
     plt.show()
-
 
     a = round(min(kwiatki, 2), 0)
     b = round(max(kwiatki, 2), 0)
@@ -137,9 +140,9 @@ def rysujHistogram(kwiatki):
     virginica = []
     nazwy = ["Setosa", "Versicolor", "Virginica"]
     for x in kwiatki:
-        if(int(x[4]) == 0):
+        if (int(x[4]) == 0):
             setosa.append(float(x[0]))
-        elif(int(x[4]) == 1):
+        elif (int(x[4]) == 1):
             versicolor.append(float(x[0]))
         else:
             virginica.append(float(x[0]))
@@ -157,9 +160,9 @@ def rysujHistogram(kwiatki):
     versicolor = []
     virginica = []
     for x in kwiatki:
-        if(int(x[4]) == 0):
+        if (int(x[4]) == 0):
             setosa.append(float(x[1]))
-        elif(int(x[4]) == 1):
+        elif (int(x[4]) == 1):
             versicolor.append(float(x[1]))
         else:
             virginica.append(float(x[1]))
@@ -213,6 +216,7 @@ def rysujHistogram(kwiatki):
     plt.ylabel("Liczebność")
     plt.show()
 
+
 def rysujPudelkowy(kwiatki):
     setosa = []
     versicolor = []
@@ -230,7 +234,7 @@ def rysujPudelkowy(kwiatki):
     bp = ax.boxplot(razem)
     plt.ylabel("Długość[cm]")
     plt.xlabel("Gatunek")
-    plt.xticks([1,2,3], nazwy)
+    plt.xticks([1, 2, 3], nazwy)
     plt.show()
 
     setosa = []
@@ -297,43 +301,45 @@ for row in csv:
 
 f.close()
 
-liczebnosc()
+print("Liczebność gatunku setosa = ", findQuantity(kwiatki, 4, 0))
+print("Liczebność gatunku versicolor = ", findQuantity(kwiatki, 4, 1))
+print("Liczebność gatunku virginica = ", findQuantity(kwiatki, 4, 2))
+
+print("\t")
+print("Minimum dlugosci dzialki kielicha: ", '%.2f' %findMinimum(kwiatki, 0))
+print("Maksimum dlugosci dzialki kielicha: ", '%.2f' % findMaximum(kwiatki, 0))
+print("Srednia arytmetyczna dlugosci dzialki kielicha: ", '%.2f' % findAverage(kwiatki, 0))
+print("Mediana dlugosci dzialki kielicha: ", '%.2f' % findMedian(kwiatki, 0))
+print("Kwartl 1: ", '%.2f' % findQ1(kwiatki, 0))
+print("Kwartl 3: ", '%.2f' % findQ3(kwiatki, 0))
+print("Odchylenie standardowe: ", '%.2f' % findStdDeviation(kwiatki, 0))
 print("\t")
 
-print("Minimum dlugosci dzialki kielicha: ", '%.2f' % min(kwiatki, 0))
-print("Maksimum dlugosci dzialki kielicha: ", '%.2f' % max(kwiatki, 0))
-print("Srednia arytmetyczna dlugosci dzialki kielicha: ", '%.2f' % srednia_arytmetyczna(kwiatki, 0))
-print("Mediana dlugosci dzialki kielicha: ", '%.2f' % mediana(kwiatki, 0))
-print("Kwartl 1: ", '%.2f' % kwartylQ1(kwiatki, 0))
-print("Kwartl 3: ", '%.2f' % kwartylQ3(kwiatki, 0))
-print("Odchylenie standardowe: ", '%.2f' % odchylenieStandardowe(kwiatki, 0))
+print("Minimum szerokosci dzialki kielicha: ", '%.2f' % findMinimum(kwiatki, 1))
+print("Maksimum szerokosci dzialki kielicha: ", '%.2f' % findMaximum(kwiatki, 1))
+print("Srednia arytmetyczna szerokosci dzialki kielicha: ", '%.2f' % findAverage(kwiatki, 1))
+print("Mediana szerokosci dzialki kielicha: ", '%.2f' % findMedian(kwiatki, 1))
+print("Kwartl 1: ", '%.2f' % findQ1(kwiatki, 1))
+print("Kwartl 3: ", '%.2f' % findQ3(kwiatki, 1))
+print("Odchylenie standardowe: ", '%.2f' % findStdDeviation(kwiatki, 1))
 print("\t")
 
-print("Minimum szerokosci dzialki kielicha: ", '%.2f' % min(kwiatki, 1))
-print("Maksimum szerokosci dzialki kielicha: ", '%.2f' % max(kwiatki, 1))
-print("Srednia arytmetyczna szerokosci dzialki kielicha: ", '%.2f' % srednia_arytmetyczna(kwiatki, 1))
-print("Mediana szerokosci dzialki kielicha: ", '%.2f' % mediana(kwiatki, 1))
-print("Kwartl 1: ", '%.2f' % kwartylQ1(kwiatki, 1))
-print("Kwartl 3: ", '%.2f' % kwartylQ3(kwiatki, 1))
-print("Odchylenie standardowe: ", '%.2f' % odchylenieStandardowe(kwiatki, 1))
+print("Minimum dlugosci platka: ", '%.2f' % findMinimum(kwiatki, 2))
+print("Maksimum dlugosci platka: ", '%.2f' % findMaximum(kwiatki, 2))
+print("Srednia arytmetyczna dlugosci platka: ", '%.2f' % findAverage(kwiatki, 2))
+print("Mediana dlugosci platka: ", '%.2f' % findMedian(kwiatki, 2))
+print("Kwartyl 1: ", '%.2f' % findQ1(kwiatki, 2))
+print("Kwartyl 3: ", '%.2f' % findQ3(kwiatki, 2))
+print("Odchylenie standardowe: ", '%.2f' % findStdDeviation(kwiatki, 2))
 print("\t")
 
-print("Minimum dlugosci platka: ", '%.2f' % min(kwiatki, 2))
-print("Maksimum dlugosci platka: ", '%.2f' % max(kwiatki, 2))
-print("Srednia arytmetyczna dlugosci platka: ", '%.2f' % srednia_arytmetyczna(kwiatki, 2))
-print("Mediana szerokosci dzialki kielicha: ", '%.2f' % mediana(kwiatki, 2))
-print("Kwartl 1: ", '%.2f' % kwartylQ1(kwiatki, 2))
-print("Kwartl 3: ", '%.2f' % kwartylQ3(kwiatki, 2))
-print("Odchylenie standardowe: ", '%.2f' % odchylenieStandardowe(kwiatki, 2))
-print("\t")
-
-print("Minimum szerokosci platka: ", '%.2f' % min(kwiatki, 3))
-print("Maksimum szerokosci platka: ", '%.2f' % max(kwiatki, 3))
-print("Srednia arytmetyczna szerokosci platka: ", '%.2f' % srednia_arytmetyczna(kwiatki, 3))
-print("Mediana szerokosci dzialki kielicha: ", '%.2f' % mediana(kwiatki, 3))
-print("Kwartl 1: ", '%.2f' % kwartylQ1(kwiatki, 3))
-print("Kwartl 3: ", '%.2f' % kwartylQ3(kwiatki, 3))
-print("Odchylenie standardowe: ", '%.2f' % odchylenieStandardowe(kwiatki, 3))
+print("Minimum szerokosci platka: ", '%.2f' % findMinimum(kwiatki, 3))
+print("Maksimum szerokosci platka: ", '%.2f' % findMaximum(kwiatki, 3))
+print("Srednia arytmetyczna szerokosci platka: ", '%.2f' % findAverage(kwiatki, 3))
+print("Mediana szerokosci dzialki kielicha: ", '%.2f' % findMedian(kwiatki, 3))
+print("Kwartyl 1: ", '%.2f' % findQ1(kwiatki, 3))
+print("Kwartyl 3: ", '%.2f' % findQ3(kwiatki, 3))
+print("Odchylenie standardowe: ", '%.2f' % findStdDeviation(kwiatki, 3))
 
 # rysujHistogram(kwiatki)
 # rysujPudelkowy(kwiatki)
