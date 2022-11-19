ca9176f2d0d7216ba9208d9607f522e32a7b777a
242456 Łaski Robert <242456@edu.p.lodz.pl>
2022-11-19 10:53:42 +0100

    działa i kod zajebisty, boże jaki dobry

--- kad.py
+++ kad.py
@@ -1,8 +1,6 @@
 import csv
 from matplotlib import pyplot as plt
 import math
-
-
 def findMinimum(table, column):
     min = float(table[0][column])
     for x in kwiatki:
@@ -14,7 +12,7 @@ def findMinimum(table, column):
 def findMaximum(table, column):
     max = float(table[0][column])
     for x in kwiatki:
-        if (float(x[column]) > max):
+        if float(x[column]) > max:
             max = float(x[column])
     return max
 
@@ -22,8 +20,8 @@ def findMaximum(table, column):
 def findQuantity(table, column, searched_value):
     quantity_searched_value = 0
     for x in table:
-        if (int(x[column]) == searched_value):
-            quantity_searched_value = quantity_searched_value+1
+        if int(x[column]) == searched_value:
+            quantity_searched_value = quantity_searched_value + 1
     return quantity_searched_value
 
 
@@ -31,7 +29,7 @@ def findAverage(table, column):
     sum = 0.0
     for x in table:
         sum += float(x[column])
-    return (sum / len(table))
+    return sum / len(table)
 
 
 def findMedian(table, column):
@@ -39,45 +37,41 @@ def findMedian(table, column):
     for x in table:
         table_to_sort.append(float(x[column]))
     table_to_sort.sort()
-    middle_value = int(len(table_to_sort)/2)
-    if(len(table_to_sort) % 2 == 0):
-        return ((table_to_sort[middle_value-1]+table_to_sort[middle_value])/2)
-    if(len(table_to_sort) % 2 == 1):
-        return middle_value;
-
-
+    middle_value = int(len(table_to_sort) / 2)
+    if len(table_to_sort) % 2 == 0:
+        return (table_to_sort[middle_value - 1] + table_to_sort[middle_value]) / 2
+    if len(table_to_sort) % 2 == 1:
+        return middle_value
 
 
 def findQ1(table, column):
     Q1 = []
     for x in table:
         Q1.append(float(x[column]))
-    n = len(table)
     Q1.sort()
-    if (len(Q1) % 4 == 0):
-        return Q1[int(len(Q1)/4)]-(Q1[int(len(Q1)/4)]-Q1[int(len(Q1)/4)-1])/4
-    if (len(Q1) % 4 == 1):
-        return Q1[int(len(Q1)/4)]
-    if (len(Q1) % 4 == 2):
-        return Q1[int(len(Q1)/4)]+(Q1[int(len(Q1)/4)+1]-Q1[int(len(Q1)/4)])/4
-    if (len(Q1) % 4 == 3):
-        return (Q1[int(len(Q1)/4)]+Q1[int(len(Q1)/4)+1])/2
+    if len(Q1) % 4 == 0:
+        return Q1[int(len(Q1) / 4)] - (Q1[int(len(Q1) / 4)] - Q1[int(len(Q1) / 4) - 1]) / 4
+    if len(Q1) % 4 == 1:
+        return Q1[int(len(Q1) / 4)]
+    if len(Q1) % 4 == 2:
+        return Q1[int(len(Q1) / 4)] + (Q1[int(len(Q1) / 4) + 1] - Q1[int(len(Q1) / 4)]) / 4
+    if len(Q1) % 4 == 3:
+        return (Q1[int(len(Q1) / 4)] + Q1[int(len(Q1) / 4) + 1]) / 2
 
 
 def findQ3(table, column):
     Q3 = []
     for x in table:
         Q3.append(float(x[column]))
-
     Q3.sort()
-    if (len(Q3) % 4 == 0):
-        return Q3[int(len(Q3) / 4*3)] - (Q3[int(len(Q3) / 4*3)] - Q3[int(len(Q3) / 4*3) - 1]) / 4*3
-    if (len(Q3) % 4 == 1):
-        return Q3[int(len(Q3) / 4*3)]
-    if (len(Q3) % 4 == 2):
-        return Q3[int(len(Q3) / 4*3)] - (Q3[int(len(Q3) / 4*3)] - Q3[int(len(Q3) / 4*3)-1]) / 4
-    if (len(Q3) % 4 == 3):
-        return (Q3[int(len(Q3) / 4*3)] + Q3[int(len(Q3) / 4*3) - 1]) / 2
+    if len(Q3) % 4 == 0:
+        return Q3[int(len(Q3) / 4 * 3)] - (Q3[int(len(Q3) / 4 * 3)] - Q3[int(len(Q3) / 4 * 3) - 1]) / 4 * 3
+    if len(Q3) % 4 == 1:
+        return Q3[int(len(Q3) / 4 * 3)]
+    if len(Q3) % 4 == 2:
+        return Q3[int(len(Q3) / 4 * 3)] - (Q3[int(len(Q3) / 4 * 3)] - Q3[int(len(Q3) / 4 * 3) - 1]) / 4
+    if len(Q3) % 4 == 3:
+        return (Q3[int(len(Q3) / 4 * 3)] + Q3[int(len(Q3) / 4 * 3) - 1]) / 2
 
 
 def findStdDeviation(table, column):
@@ -85,221 +79,68 @@ def findStdDeviation(table, column):
     sum = 0
     for x in table:
         sum += (float(float(x[column]) - avg) ** 2)
-
     sum /= len(table)
     sum = sum ** 0.5
     return sum
 
 
-def rysujHistogram(kwiatki):
-    dlugosc_dzialki = []
-    szerokosc_dzialki = []
-    dlugosc_platka = []
-    szerokosc_platka = []
-    for x in kwiatki:
-        dlugosc_dzialki.append(float(x[0]))
-        szerokosc_dzialki.append(float(x[1]))
-        dlugosc_platka.append(float(x[2]))
-        szerokosc_platka.append(float(x[3]))
-
-    a = round(min(kwiatki, 0), 0)
-    b = round(max(kwiatki, 0), 0)
-    bins = int(round(math.sqrt(len(dlugosc_dzialki)), 0))
-    plt.hist(dlugosc_dzialki, bins=bins, range=(a, b), edgecolor='black')
-    plt.title("Długość działki kielicha")
-    plt.xlabel("Długość[cm]")
+def drawHistogram(table, column, title, xlable):
+    table_values = []
+    for x in table:
+        table_values.append(float(x[column]))
+    min = round(findMinimum(table, column), 0)
+    max = round(findMaximum(table, column), 0)
+    bins = int(round(math.sqrt(len(table_values)), 0))
+    plt.hist(table_values, bins=bins, range=(min, max), edgecolor='black')
+    plt.title(title)
     plt.ylabel("Liczebność")
+    plt.xlabel(xlable)
     plt.show()
 
-    a = round(min(kwiatki, 1), 0)
-    b = round(max(kwiatki, 1), 0)
-    plt.hist(szerokosc_dzialki, bins=bins, range=(a, b), edgecolor='black')
-    plt.title("Szerokość działki kielicha")
-    plt.xlabel("Szerokość[cm]")
-    plt.ylabel("Liczebność")
-    plt.show()
+def SortTableForTables(table, column):
+    a_category = []
+    b_category = []
+    c_category = []
+    for x in table:
+        if int(x[4]) == 0:
+            a_category.append(float(x[column]))
+        if int(x[4]) == 1:
+            b_category.append(float(x[column]))
+        if int(x[4]) == 2:
+            c_category.append(float(x[column]))
+    return [a_category, b_category, c_category]
 
-    a = round(min(kwiatki, 2), 0)
-    b = round(max(kwiatki, 2), 0)
-    plt.hist(dlugosc_platka, bins=bins, range=(a, b), edgecolor='black')
-    plt.title("Długość płatka")
-    plt.xlabel("Długość[cm]")
-    plt.ylabel("Liczebność")
-    plt.show()
-
-    a = round(min(kwiatki, 3), 0)
-    b = round(max(kwiatki, 3), 0)
-    plt.hist(szerokosc_platka, bins=bins, range=(a, b), edgecolor='black')
-    plt.title("Szerokość płatka")
-    plt.xlabel("Szerokość[cm]")
-    plt.ylabel("Liczebność")
-    plt.show()
-
-    setosa = []
-    versicolor = []
-    virginica = []
-    nazwy = ["Setosa", "Versicolor", "Virginica"]
-    for x in kwiatki:
-        if (int(x[4]) == 0):
-            setosa.append(float(x[0]))
-        elif (int(x[4]) == 1):
-            versicolor.append(float(x[0]))
-        else:
-            virginica.append(float(x[0]))
-    razem = [setosa, versicolor, virginica]
-    a = round(min(kwiatki, 0), 0)
-    b = round(max(kwiatki, 0), 0)
-    plt.hist(razem, bins=bins, range=(a, b), edgecolor='black', stacked=True, label=nazwy)
+def drawHistogramFor3Species(table, column, title, xlabel, names_species):
+    table_for_all_categories = SortTableForTables(table, column)
+    min = round(findMinimum(table, column), 0)
+    max = round(findMaximum(table, column), 0)
+    bins = int(round(math.sqrt(len(table)), 0))
+    plt.hist(table_for_all_categories, bins=bins, range=(min, max), edgecolor='black', stacked=True,
+             label=names_species)
     plt.legend(loc="upper left")
-    plt.title("Długość działki kielicha")
-    plt.xlabel("Długość[cm]")
+    plt.title(title)
+    plt.xlabel(xlabel)
     plt.ylabel("Liczebność")
     plt.show()
 
-    setosa = []
-    versicolor = []
-    virginica = []
-    for x in kwiatki:
-        if (int(x[4]) == 0):
-            setosa.append(float(x[1]))
-        elif (int(x[4]) == 1):
-            versicolor.append(float(x[1]))
-        else:
-            virginica.append(float(x[1]))
-    razem = [setosa, versicolor, virginica]
-    a = round(min(kwiatki, 1), 0)
-    b = round(max(kwiatki, 1), 0)
-    plt.hist(razem, bins=bins, range=(a, b), edgecolor='black', stacked=True, label=nazwy)
-    plt.legend(loc="upper left")
-    plt.title("Szerokość działki kielicha")
-    plt.xlabel("Szerokość[cm]")
-    plt.ylabel("Liczebność")
-    plt.show()
-
-    setosa = []
-    versicolor = []
-    virginica = []
-    for x in kwiatki:
-        if (int(x[4]) == 0):
-            setosa.append(float(x[2]))
-        elif (int(x[4]) == 1):
-            versicolor.append(float(x[2]))
-        else:
-            virginica.append(float(x[2]))
-    razem = [setosa, versicolor, virginica]
-    a = round(min(kwiatki, 2), 0)
-    b = round(max(kwiatki, 2), 0)
-    plt.hist(razem, bins=bins, range=(a, b), edgecolor='black', stacked=True, label=nazwy)
-    plt.legend(loc="upper left")
-    plt.title("Długość płatka")
-    plt.xlabel("Długość[cm]")
-    plt.ylabel("Liczebność")
-    plt.show()
-
-    setosa = []
-    versicolor = []
-    virginica = []
-    for x in kwiatki:
-        if (int(x[4]) == 0):
-            setosa.append(float(x[3]))
-        elif (int(x[4]) == 1):
-            versicolor.append(float(x[3]))
-        else:
-            virginica.append(float(x[3]))
-    razem = [setosa, versicolor, virginica]
-    a = round(min(kwiatki, 3), 0)
-    b = round(max(kwiatki, 3), 0)
-    plt.hist(razem, bins=bins, range=(a, b), edgecolor='black', stacked=True, label=nazwy)
-    plt.legend(loc="upper left")
-    plt.title("Szerokość płatka")
-    plt.xlabel("Szerokość[cm]")
-    plt.ylabel("Liczebność")
-    plt.show()
-
-
-def rysujPudelkowy(kwiatki):
-    setosa = []
-    versicolor = []
-    virginica = []
-    nazwy = ["Setosa", "Versicolor", "Virginica"]
-    for x in kwiatki:
-        if (int(x[4]) == 0):
-            setosa.append(float(x[0]))
-        elif (int(x[4]) == 1):
-            versicolor.append(float(x[0]))
-        else:
-            virginica.append(float(x[0]))
-    razem = [setosa, versicolor, virginica]
+def drawBoxPlotFor3Species(table, column, names_species, ylabel):
+    table_for_all_categories = SortTableForTables(table, column)
     fig, ax = plt.subplots()
-    bp = ax.boxplot(razem)
-    plt.ylabel("Długość[cm]")
+    bp = ax.boxplot(table_for_all_categories)
     plt.xlabel("Gatunek")
-    plt.xticks([1, 2, 3], nazwy)
-    plt.show()
-
-    setosa = []
-    versicolor = []
-    virginica = []
-    for x in kwiatki:
-        if (int(x[4]) == 0):
-            setosa.append(float(x[1]))
-        elif (int(x[4]) == 1):
-            versicolor.append(float(x[1]))
-        else:
-            virginica.append(float(x[1]))
-    razem = [setosa, versicolor, virginica]
-    fig, ax = plt.subplots()
-    bp = ax.boxplot(razem)
-    plt.ylabel("Szerokość[cm]")
-    plt.xlabel("Gatunek")
-    plt.xticks([1, 2, 3], nazwy)
-    plt.show()
-
-    setosa = []
-    versicolor = []
-    virginica = []
-    for x in kwiatki:
-        if (int(x[4]) == 0):
-            setosa.append(float(x[2]))
-        elif (int(x[4]) == 1):
-            versicolor.append(float(x[2]))
-        else:
-            virginica.append(float(x[2]))
-    razem = [setosa, versicolor, virginica]
-    fig, ax = plt.subplots()
-    bp = ax.boxplot(razem)
-    plt.ylabel("Długość[cm]")
-    plt.xlabel("Gatunek")
-    plt.xticks([1, 2, 3], nazwy)
-    plt.show()
-
-    setosa = []
-    versicolor = []
-    virginica = []
-    for x in kwiatki:
-        if (int(x[4]) == 0):
-            setosa.append(float(x[3]))
-        elif (int(x[4]) == 1):
-            versicolor.append(float(x[3]))
-        else:
-            virginica.append(float(x[3]))
-    razem = [setosa, versicolor, virginica]
-    fig, ax = plt.subplots()
-    bp = ax.boxplot(razem)
-    plt.ylabel("Szerokość[cm]")
-    plt.xlabel("Gatunek")
-    plt.xticks([1, 2, 3], nazwy)
+    plt.ylabel(ylabel)
+    plt.xticks([1, 2, 3], names_species)
     plt.show()
 
 
 f = open(r"data.csv")
 csv = csv.reader(f)
 kwiatki = []
-
 for row in csv:
     kwiatki.append(row)
-
 f.close()
+
+nazwy = ["setosa", "versicolor", "virginica"]
 
 print("Liczebność gatunku setosa = ", findQuantity(kwiatki, 4, 0))
 print("Liczebność gatunku versicolor = ", findQuantity(kwiatki, 4, 1))
@@ -341,5 +182,17 @@ print("Kwartyl 1: ", '%.2f' % findQ1(kwiatki, 3))
 print("Kwartyl 3: ", '%.2f' % findQ3(kwiatki, 3))
 print("Odchylenie standardowe: ", '%.2f' % findStdDeviation(kwiatki, 3))
 
-# rysujHistogram(kwiatki)
-# rysujPudelkowy(kwiatki)
+drawHistogram(kwiatki, 0, "Długość działki kielicha", "Długość[cm]")
+drawHistogram(kwiatki, 1, "Szerokość działki kielicha", "Szerokość[cm]")
+drawHistogram(kwiatki, 2, "Długość płatka", "Długość[cm]")
+drawHistogram(kwiatki, 3, "Szerokość płatka", "Szerokość[cm]")
+
+drawBoxPlotFor3Species(kwiatki, 0, nazwy, "Długośc[cm]")
+drawBoxPlotFor3Species(kwiatki, 1, nazwy, "Szerokość[cm]")
+drawBoxPlotFor3Species(kwiatki, 2, nazwy, "Długość[cm]")
+drawBoxPlotFor3Species(kwiatki, 3, nazwy, "Szerokośc[cm]")
+
+drawHistogramFor3Species(kwiatki, 0, "Długość działki kielicha", "Długość[cm]", nazwy)
+drawHistogramFor3Species(kwiatki, 1, "Szerokość działki kielicha", "Szerokość[cm]", nazwy)
+drawHistogramFor3Species(kwiatki, 2, "Długość płatka", "Długość[cm]", nazwy)
+drawHistogramFor3Species(kwiatki, 3, "Szerokość płatka", "Szerokość[cm]", nazwy)
